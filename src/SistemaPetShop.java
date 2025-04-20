d57c4a4f5c97aab7674f58679aefd785c0f65f28
w1shey <wisheydpftn@gmail.com>
2025-04-20 18:29:50 -0300

    MINHA PARTE REI DA MIDIA 22

--- src/SistemaPetShop.java
+++ src/SistemaPetShop.java
@@ -67,3 +67,77 @@ abstract class Servico {
     }
 }
 
+class BanhoETosa extends Servico {
+    public BanhoETosa(Date data) {
+        super("Banho e Tosa", data);
+    }
+
+    @Override
+    public double getPreco() {
+        return 50.0;
+    }
+}
+
+class ConsultaVeterinaria extends Servico {
+    public ConsultaVeterinaria(Date data) {
+        super("Consulta Veterinária", data);
+    }
+
+    @Override
+    public double getPreco() {
+        return 100.0;
+    }
+}
+
+class PacoteServicos {
+    private List<Servico> servicos = new ArrayList<>();
+
+    public void adicionarServico(Servico s) {
+        servicos.add(s);
+    }
+
+    public double getPrecoTotal() {
+        double total = 0;
+        for (Servico s : servicos) {
+            total += s.getPreco();
+        }
+        return total * 0.9; // 10% de desconto
+    }
+
+    public List<Servico> getServicos() { return servicos; }
+
+    @Override
+    public String toString() {
+        return "Pacote: " + servicos + ", Total com desconto: R$" + getPrecoTotal();
+    }
+}
+public class SistemaPetShop {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente("Gabriel", "(61)99848-2810", "gabrielbigorna969@gamil.com");
+        Pet pet = new Pet("Ozzy", "Cachorro", "Dog Alemao", 3, 70.0);
+
+
+        LocalDate localDate = LocalDate.of(2026, 5, 20);
+        Date dataAgendada = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant()); 
+
+        cliente.adicionarPet(pet);
+
+
+
+
+        Date hoje = new Date();
+        Servico banho = new BanhoETosa(hoje);
+        Servico consulta = new ConsultaVeterinaria(hoje);
+
+        PacoteServicos pacote = new PacoteServicos();
+        pacote.adicionarServico(banho);
+        pacote.adicionarServico(consulta);
+
+
+
+
+        System.out.println("Cliente: " + cliente);
+        System.out.println("Pet: " + pet);
+
+        System.out.println(pacote);
+
